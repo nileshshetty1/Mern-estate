50f2d4fd7fbb5820d0165d6ff561c3423588e26e
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-08 21:09:28 +0530

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Signup from "./pages/Signup";
 import Signout from "./pages/Signout";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/sign-out" element={<Signout />} />
           <Route path="/about" element={<About />} />

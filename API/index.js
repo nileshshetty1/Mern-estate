8335a59f95c5783cd98da152640a75f407b5b32b
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-09 17:58:38 +0530

    created the authcontrollers to handle the input from the client and store it in the mongoDB

--- API/index.js
+++ API/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
+import authRouter from "./routes/authRoute.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -13,7 +14,10 @@ mongoose
   });
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");

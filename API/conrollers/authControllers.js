2d8b203467029aa26eb189845a6e360d0d186d04
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-10 17:53:12 +0530

    created signin api route

--- API/conrollers/authControllers.js
+++ API/conrollers/authControllers.js
@@ -1,5 +1,8 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -7,6 +10,24 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully.");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) return next(errorHandler(404, "User not Found"));
+    const validPassword = bcrypt.compareSync(password, validuser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validuser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true }) // this means that no other third party can access the token.
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- API/routes/authRoute.js
+++ API/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../conrollers/authControllers.js";
+import { signin, signup } from "../conrollers/authControllers.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;

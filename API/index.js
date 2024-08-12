ea5e916f08e5f5640a919647e0ed8b2e2d704145
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-12 23:13:39 +0530

    create update api route

--- API/index.js
+++ API/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoute.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,7 +16,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
--- API/routes/userRoutes.js
+++ API/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { test } from "../conrollers/userControllers.js";
+import { test, updateUser } from "../conrollers/userControllers.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;

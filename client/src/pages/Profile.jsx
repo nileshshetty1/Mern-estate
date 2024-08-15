0c18baea1198573b739614820a25f0c72d2fb3c0
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-15 23:48:37 +0530

    add user delete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,6 +9,9 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase.js";
 import {
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
   signInFailure,
   updateUserFailure,
   updateUserStart,
@@ -80,6 +83,23 @@ const Profile = () => {
       dispatch(updateUserFailure(error.message));
     }
   };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -142,12 +162,14 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 hover:cursor-pointer">
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 hover:cursor-pointer"
+        >
           Delete account
         </span>
         <span className="text-red-700 hover:cursor-pointer">Sign out</span>
       </div>
-      <p className="text-red-700">{error ? error : ""}</p>
       <p className="text-green-700">
         {updateSuccess ? "User is updated successfully" : ""}
       </p>

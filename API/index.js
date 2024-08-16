e85e0b533e513c3117e01491076a93e67a916fc1
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-16 11:18:34 +0530

    created listing route and model

--- API/index.js
+++ API/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/lisitngRoute.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;

c142ca75f2446624ae0485b446e55e1a22c9cdb5
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-17 15:56:45 +0530

    Created get user Listings API route

--- API/models/listingModal.js
+++ API/models/listingModal.js
@@ -1,3 +1,67 @@
+// import mongoose from "mongoose";
+
+// const ListingSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     description: {
+//       type: String,
+//       required: true,
+//     },
+//     address: {
+//       type: String,
+//       required: true,
+//     },
+//     regularPrice: {
+//       type: Number,
+//       required: true,
+//     },
+//     discountPrice: {
+//       type: Number,
+//       required: true,
+//     },
+//     bathrooms: {
+//       type: Number,
+//       required: true,
+//     },
+//     bedrooms: {
+//       type: Number,
+//       required: true,
+//     },
+//     furnished: {
+//       type: Boolean,
+//       required: true,
+//     },
+//     parking: {
+//       type: Boolean,
+//       required: true,
+//     },
+//     type: {
+//       type: String,
+//       required: true,
+//     },
+//     offer: {
+//       type: Boolean,
+//       required: true,
+//     },
+//     imageUrls: {
+//       type: Array,
+//       required: true,
+//     },
+//     userRef: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// const Listing = mongoose.model("Listing", ListingSchema);
+
+// export default Listing;
+
 import mongoose from "mongoose";
 
 const ListingSchema = new mongoose.Schema(
@@ -58,6 +122,8 @@ const ListingSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Listing = mongoose.model("Listing", ListingSchema);
+// Check if the model already exists to prevent OverwriteModelError
+const Listing =
+  mongoose.models.Listing || mongoose.model("Listing", ListingSchema);
 
 export default Listing;

a96cdefb7f3c6d01214c70fbef213ce558d54a3d
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-17 17:57:59 +0530

    created get user api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -29,6 +29,7 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     if (file) {
@@ -118,6 +119,20 @@ const Profile = () => {
       dispatch(signoutUserFailure(data.message));
     }
   };
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -201,6 +216,12 @@ const Profile = () => {
       </div>
       <p className="text-green-700">
         {updateSuccess ? "User is updated successfully" : ""}
+      </p>
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "Error Showing Listings" : ""}
       </p>
     </div>
   );

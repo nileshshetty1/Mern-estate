e5a8a1e24c871d69cea5a195c9b1f68d038634cf
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-21 14:13:55 +0530

    created search API route

--- API/routes/lisitngRoute.js
+++ API/routes/lisitngRoute.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../conrollers/listingControllers.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,5 +14,5 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", verifyToken, getListing);
-
+router.get("/get", getListings);
 export default router;

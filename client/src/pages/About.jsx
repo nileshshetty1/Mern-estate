d4711d586c59ae8e1bd48ed3e5f9622ca046f181
Nilesh Shetty <nileshshetty45@gmail.com>
2024-08-22 20:42:31 +0530

    Complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Royal Estate
+      </h1>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit laudantium
+        cumque ab soluta nemo, sint blanditiis ullam odit obcaecati voluptates
+        molestiae cupiditate tempora rem, totam officia sapiente suscipit
+        debitis accusantium!
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab repellat
+        explicabo quaerat aperiam mollitia incidunt fugit. Eum aut assumenda
+        asperiores nemo. Fugiat dolore nesciunt, ea minus non voluptatum
+        adipisci ut?
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, culpa
+        maxime. Vitae nesciunt cupiditate incidunt eos. Rem sit consectetur
+        velit neque odio?
+      </p>
+    </div>
+  );
 };
 
 export default About;
